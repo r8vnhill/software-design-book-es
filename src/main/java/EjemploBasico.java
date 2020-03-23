a190c6feb6b6e00132bbd6d3766dea0ca5a6da0a
SL8R <ignacio.slater@ug.uchile.cl>
2020-03-23 15:07:25 -0300

    Introducción

--- src/main/java/EjemploBasico.java
+++ src/main/java/EjemploBasico.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EjemploBasico {
+
+  public static void main(String[] args) {
+    System.out.println("Write your name: ");
+    String name = new Scanner(System.in).nextLine();
+    System.out.println("My name is " + name + ", and I have a dream.");
+  }
 }
