24186adb672b0b1f9707c82d3745ea0e65b06002
Roäc <smflako@gmail.com>
2020-04-01 11:55:49 -0300

    Git for Windows

--- src/main/java/Vector2D.java
+++ src/main/java/Vector2D.java
@@ -7,14 +7,9 @@ class Vector2D extends VectorND {
     super(new double[] {x, y});
   }
 
-//  public static void main(String[] args) {
-//    Vector2D point = new Vector2D(1, 3);
-//    Vector2D newPoint = point.add(new Vector2D(-1, 2));
-//    newPoint.print();
-//  }
-//
-//
-//  void print() {
-//    System.out.println("(" + x + ", " + y + ")");
-//  }
+  public static void main(String[] args) {
+    Vector2D point = new Vector2D(1, 3);
+    VectorND newPoint = point.add(new Vector2D(-1, 2));
+    newPoint.print();
+  }
 }
