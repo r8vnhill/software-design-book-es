b5e707299054a7475a83bb2b767b4a42809a4f80
SL8R <islaterm@gmail.com>
2021-02-25 17:42:00 -0300

    JDK Linux

---
+++ install/windows/cmder.ps1
@@ -0,0 +1,8 @@
+cinst cmder -y
+
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name 'oh-my-posh'
+Install-Module -Name 'Get-ChildItemColor'
+
+ise $PROFILE
