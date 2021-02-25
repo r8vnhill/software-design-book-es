7b53a8d87d4e4283c51122eaf0229d81f9253810
SL8R <islaterm@gmail.com>
2021-02-25 15:35:28 -0300

    Chocolatey

--- install/windows/chocolatey.ps1
+++ install/windows/chocolatey.ps1
@@ -1,3 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
 Invoke-WebRequest "https://chocolatey.org/install.ps1" -UseBasicParsing | Invoke-Expression
+choco -?
